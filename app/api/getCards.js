a16bc4bd6ac3997b8c5a0eb9731c35d74ff00bfb
Lukas Maynard <104531583+Lukas-Maynard@users.noreply.github.com>
2024-10-24 23:08:10 -0400

    vercel deployment issues

--- app/api/getCards.js
+++ app/api/getCards.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
       res.status(200).json(cards);
     } catch (error) {
       res.status(500).json({ error: 'Failed to fetch cards' });
+      console.log(error)
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from 'next/link';
 import CardListingPage from './CardListingPage';
 import AddCardPage from './addCard/page';
 
@@ -7,13 +6,6 @@ export default function HomePage(){
   return (
     <div>
       <h1>Welcome to Flashcard App</h1>
-      {/* <Link href="/">Home</Link>
-      <br></br>
-      <Link href="/cards">Go to Card Listing</Link>
-      <br></br>
-      <Link href="/addCard">Add Cards</Link> */}
-
-      <br></br>
       <br></br>
       <CardListingPage/>
       <br></br>
